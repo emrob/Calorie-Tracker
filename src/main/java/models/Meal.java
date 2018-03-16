71490b564a0bd30ac46e922704f788c2bcf9add6
Emma Roberts <emrsixtyseven@gmail.com>
2018-03-16 13:00:10 +0000

    Meal class added

--- src/main/java/models/Meal.java
+++ src/main/java/models/Meal.java
@@ -1,4 +1,34 @@
 package models;
 
+import behaviours.IConsume;
+
+import java.util.ArrayList;
+
 public class Meal {
+
+    private String name;
+    private ArrayList<IConsume> meals;
+
+    public Meal(String name) {
+        this.name = name;
+        this.meals = new ArrayList<IConsume>();
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public ArrayList<IConsume> getMeals() {
+        return meals;
+    }
+
+    public void setMeals(ArrayList<IConsume> meals) {
+        this.meals = meals;
+    }
+    
 }
